5024cfc4bf7e4421217dab79faa3933de1b04b73
ErnestBoz <ErnestBoz>
2024-11-06 20:22:57 +0000

    made player-deck topology

--- CardImplementor.java
+++ CardImplementor.java
@@ -6,24 +6,44 @@ public class CardImplementor {
     private ArrayList<Card> myCards = new ArrayList<>();
 
     public void createPlayers(int nPlayer) {
-        List<Deck> decks = new ArrayList<>();
+
         
+        myDecks.clear();
+        myPlayers.clear();
+
         // Create the decks
         for (int i = 0; i < nPlayer; i++) {
-            decks.add(new Deck());
+            myDecks.add(new Deck());
         }
         
         // Create the players with circular deck references
         for (int i = 0; i < nPlayer; i++) {
-            Deck leftDeck = decks.get((i - 1 + nPlayer) % nPlayer);
-            Deck rightDeck = decks.get((i + 1) % nPlayer);
+            Deck leftDeck;
+            Deck rightDeck;
+    
+            if (i == 0) {
+                // Player 0's left deck is the last deck, and right deck is the first deck
+                leftDeck = myDecks.get(nPlayer - 1);
+                rightDeck = myDecks.get(0);
+            } else {
+                // For other players, left deck is at index i and right deck is at index i - 1
+                leftDeck = myDecks.get(i-1);
+                rightDeck = myDecks.get(i );
+            }
+    
             myPlayers.add(new Player(leftDeck, rightDeck));
         }
+    }
 
-    public String showPlayerDetails(int playerId){
+    public String showPlayerDetails(int playerId) {
         Player player = myPlayers.get(playerId);
         Deck leftDeck = player.getLeftDeck();
         Deck rightDeck = player.getRightDeck();
-        return ("Left deck: " + leftDeck + "Right deck " + rightDeck);
+    
+        // Retrieve and show the indices of the decks in myDecks
+        int leftDeckIndex = myDecks.indexOf(leftDeck);
+        int rightDeckIndex = myDecks.indexOf(rightDeck);
+    
+        return "Left deck: " + leftDeckIndex + ", Right deck: " + rightDeckIndex;
     }
 }
--- test.java
+++ test.java
@@ -5,6 +5,10 @@ public class test {
 
         impl.createPlayers(4);
         System.out.println(impl.showPlayerDetails(0));
+        System.out.println(impl.showPlayerDetails(1));
+        System.out.println(impl.showPlayerDetails(2));
+        System.out.println(impl.showPlayerDetails(3));
+        
 
         
 
